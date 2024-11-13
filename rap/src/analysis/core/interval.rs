02471cda4b807c47d552090074e48f7a50472cce
Coursant <24210240198@fudan.edu.cn>
2024-11-13 15:00:11 +0800

    delete apron

--- rap/src/analysis/core/interval.rs
+++ rap/src/analysis/core/interval.rs
@@ -10,35 +10,14 @@ use rustc_session::Session;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
 use std::rc::Rc;
-pub struct IntervalAnalysis<'a, 'tcx> {
+pub struct IntervalAnalysis<'tcx> {
     /// The central data structure of the compiler
     pub tcx: TyCtxt<'tcx>,
 
-    /// Represents the data associated with a compilation session for a single crate
 
-    /// The entry function of the analysis
-    pub entry_point: DefId,
-
-    /// Stores the DefIds that have been already checked, to avoid redundant check
-    pub checked_def_ids: HashSet<DefId>,
-
-    /// Stores the Heaps that have been already dropped, to detect double-free, use-after-free, etc.
-    pub dropped_heaps: HashSet<Rc<SymbolicValue>>,
-
-    /// Cache for the Weak Topological Ordering
-    pub wto_cache: WtoCache<'tcx>,
-
-    /// Cache for the name of each DefId
-    pub function_name_cache: HashMap<DefId, Rc<String>>,
-
-    /// Customized options that may change the behavior of the analysis
-    pub analysis_options: AnalysisOption,
-
-    /// Generated diagnostic messages for each DefId
-    pub diagnostics_for: DiagnosticsForDefId<'compiler>,
 }
-impl IntervalAnalysis<'tcx, '_> {
-    pub fn new<'a, 'tcx>(
+impl< 'tcx> IntervalAnalysis<'tcx> {
+    pub fn new(
         tcx: TyCtxt<'tcx>
     ) -> Self {
 
