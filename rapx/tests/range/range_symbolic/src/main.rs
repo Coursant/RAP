67c45dee2f991e605348a6c4c08837e86e4b6795
Coursant <24210240198@m.fudan.edu.cn>
2025-11-04 07:12:16 +0800

    add symbolic testcase

---
+++ rapx/tests/range/range_symbolic/src/main.rs
@@ -0,0 +1,16 @@
+fn foo1(x: i32) -> i32 {
+    let a = x + 1;
+    let y = x;
+    let mut result = 3;
+    if a >= y {
+        result =  1;
+    } else {
+        result =  0;
+    }
+    return result;
+}
+
+fn main(){
+
+    foo1(2);
+}
