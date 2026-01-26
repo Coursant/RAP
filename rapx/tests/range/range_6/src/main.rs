6eca8fb982d3fc4c4d58510e105e72eb36d6c256
Coursant <24210240198@m.fudan.edu.cn>
2026-01-26 22:26:33 +0800

    add new test

--- rapx/tests/range/range_6/src/main.rs
+++ rapx/tests/range/range_6/src/main.rs
@@ -1,32 +1,13 @@
-fn longest_zero_sequence(pieces: &[u16; 8]) -> (isize, isize) {
-    let mut longest = -1;
-    let mut longest_length = -1;
-    let mut start = -1;
-    macro_rules! finish_sequence(
-        ($end: expr) => {
-            if start >= 0 {
-                let length = $end - start;
-                if length > longest_length {
-                    longest = start;
-                    longest_length = length;
-                }
-            }
-        };
-    );
-    for i in 0..8 {
-        if pieces[i as usize] == 0 {
-            if start < 0 {
-                start = i;
-            }
-        } else {
-            finish_sequence!(i);
-            start = -1;
-        }
-    }
-    finish_sequence!(8);
-    (longest, longest + longest_length)
-}
+
 pub fn main(){
-    let pieces : [u16; 8]= [42; 8];
-    longest_zero_sequence(&pieces);
-}
+    let pieces : [u8; 8]= [42; 8];
+    let mut i = 0;
+    let len = pieces.len();
+    while i < len {
+        let val = pieces[i]; 
+        if val > 128 {
+            i += 2;
+        } else {
+            i += 1;
+        }
+    }}
